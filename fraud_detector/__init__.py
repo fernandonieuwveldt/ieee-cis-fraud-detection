361d30d6a1a8028829118819acdfb7891b7c6ce9
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2019-09-03 18:12:10 +0200

    created __init__ file with model variables

---
+++ fraud_detector/__init__.py
@@ -0,0 +1,8 @@
+"""
+Fraud detector info file
+"""
+
+from os import path
+
+INPUT_DIR = path.join(path.dirname(path.realpath(__file__)), '../data/input_data/ieee-fraud-detection/')
+OUTPUT_DIR = path.join(path.dirname(path.realpath(__file__)), '../data/output_data/')
